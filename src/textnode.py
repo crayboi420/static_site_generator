c4fa6a0ffa4c474c04b40ca603abcc754a7b220c
crayboi420 <kanavjain1999@gmail.com>
2024-04-17 22:03:58 +0200

    Worked till 3.1

--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,49 @@
+from htmlnode import *
 
 
 class TextNode:
-    
-    def __init__(self,text:str,text_type:str,url:str=None):
+
+    def __init__(self, text: str, text_type: str, url: str = None):
         self.text = text
         self.text_type = text_type
         self.url = url
-        
-    def __eq__(self,other)->bool:
-        return (self.text == other.text and 
-                self.text_type == other.text_type and
-                self.url == other.url)
-    
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
     def __repr__(self) -> str:
         return f"TextNode({self.text},{self.text_type},{self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("The type is gg")
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: str
+) -> list[TextNode]:
+
+    new_nodes: list[TextNode] = []
+
+    for node in old_nodes:
+        if node.text_type != "text":
+            new_nodes.append(node)
+    return new_nodes
